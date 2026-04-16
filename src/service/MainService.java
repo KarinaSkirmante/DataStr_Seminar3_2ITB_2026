2f544cc5ed1abb0b2b90f0d318072309adcc3f4f
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-16 12:10:34 +0300

    Add: added service testing

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,10 +1,23 @@
 package service;
 
+import datastr.MyLinkedHeap;
+
 public class MainService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyLinkedHeap<Integer> integersInHeap = new MyLinkedHeap<Integer>();
+		try
+		{
+			integersInHeap.enqueue(40);//P: 40
+			integersInHeap.enqueue(50);//P: 50(LC: 40)
+			integersInHeap.enqueue(35);//P: 50(LC: 40, RC:35)
+			integersInHeap.enqueue(99);//P:99 (LC:50, RC:35), P:50 (LC:40)
+			integersInHeap.enqueue(55);//P:99 (LC:55, RC:35), P:55 (LC:40, RC:50)
+			integersInHeap.print();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
