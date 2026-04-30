f2501be977586c35e6cf8248aafee1c38e7cacc2
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-30 10:29:52 +0300

    Edit: added testing of Patient in the MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,10 +1,12 @@
 package service;
 
 import datastr.MyLinkedHeap;
+import model.Patient;
 
 public class MainService {
 
 	public static void main(String[] args) {
+		System.out.println("----------DARBĪBAS AR INT------------");
 		MyLinkedHeap<Integer> integersInHeap = new MyLinkedHeap<Integer>();
 		try
 		{
@@ -19,6 +21,24 @@ public class MainService {
 			System.out.println("Max vērtība " + integersInHeap.dequeue());
 			integersInHeap.print();
 			//TODO notestēt vēlreiz, bet tad, kad lastNode ir uztaisīts
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("----------DARBĪBAS AR PATIENT------------");
+		
+		MyLinkedHeap<Patient> patients = new MyLinkedHeap<Patient>();
+		try
+		{
+			patients.enqueue(new Patient("Jānis", "Bērziņs", 1));//P: Janis
+			patients.enqueue(new Patient("Līga", "Jaukā", 3)); //P: Līga (LC: Jānis)
+			patients.enqueue(new Patient("Baiba", "Kalniņa", 2));//P: Līga (LC: Jānis, RC: Baiba)
+			patients.enqueue(new Patient("Juris", "Nejaukais", 5));//P: Juris (LC: Līga, RC: Baiba), P:Līga (LC:Jānis)
+			patients.print();
+			
+			System.out.println("Max pacients: " + patients.dequeue());//Juris
+			patients.print();//P: Līga (LC: Jānis, RC: Baiba)
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
